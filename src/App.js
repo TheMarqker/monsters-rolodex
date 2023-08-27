30945d09ae26a38bdacc4a6dfb96b0ff8cc86de7
Adam Marquebreuck <61724267+TheMarqker@users.noreply.github.com>
2023-08-27 08:25:45 -0400

    Update App.js
    Cleaned up code and added search from api

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ class App extends Component{
    
     this.state = {
       monsters: [],
+      searchField: ''
     };
+    console.log('constructor loads 1st');
   }
 
   componentDidMount() {
+    console.log('Component did mount, loads 3rd')
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then((users) => 
@@ -26,9 +29,25 @@ class App extends Component{
   }
 
   render() {
+    console.log('render goes 2nd and again at this.state change')
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          console.log(event.target.value);
+          const searchField = event.target.value.toLocaleLowerCase();
+
+          this.setState(() => {
+            return { searchField };
+          }
+          );
+        }} 
+        />
+        {filteredMonsters.map((monster) => {
           return (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
